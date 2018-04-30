53a1467eb2c7bdf0fe0759a541f6f5d556803965
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-30 19:39:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  position = katz_deli.index("#{name}") + 1
+  position = katz_deli.index(name) + 1
   puts "Welcome, #{name}. You are number #{position} in line."
 end
