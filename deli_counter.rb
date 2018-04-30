951fbaab20844478d071dc7f7643c086e1364e9a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-30 19:43:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length >= 1
     puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift()
   else
     puts "There is nobody waiting to be served!"
   end
