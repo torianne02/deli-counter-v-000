1532bed5dcffcd772679828223d46719fda55fa6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-30 19:29:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length >= 1
     puts "The line is currently: #{katz_deli}."
-  elsif katz_deli.length >= 1
+  elsif katz_deli.length >= 0
     puts "The line is currently empty."
   end
 end
