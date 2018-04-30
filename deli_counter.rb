60e3932a4f06e142144d5ff7ffe5c06fc17ee402
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-30 19:33:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
 katz_deli = []
 
 def line(katz_deli)
+  line_order = []
   if katz_deli.length >= 1
-    puts "The line is currently: #{katz_deli}."
+    katz_deli.each_with_index do |customer, index|
+      line_order << "#{index + 1}. #{customer}"
+    end
+    puts "The line is currently: #{line_order}."
   elsif katz_deli.length == 0 
     puts "The line is currently empty."
   end
