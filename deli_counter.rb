7b5a094dd50611b87e48fbd97779cf351d76b828
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-30 19:42:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def now_serving(katz_deli)
   if katz_deli.length >= 1
     puts "Currently serving #{katz_deli[0]}."
   else
-    puts "There is nobody waiting to be served."
+    puts "There is nobody waiting to be served!"
   end
 end
